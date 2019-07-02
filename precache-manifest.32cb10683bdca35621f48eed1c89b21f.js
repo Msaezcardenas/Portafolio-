07a318bfb1e40089854a60b84f163cd2d0a5a747
Msaezcardenas <msaezcardenas@gmail.com>
2019-07-02 12:13:42 -0400

    Updates

--- precache-manifest.32cb10683bdca35621f48eed1c89b21f.js
+++ precache-manifest.32cb10683bdca35621f48eed1c89b21f.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d610f99ed6d7ac4b67451c375061c1b6",
+    "revision": "e8f62f2055530a76f8866da27b16241c",
     "url": "/Portafolio-/index.html"
   },
   {
-    "revision": "64971d68aa9c27f1df2a",
-    "url": "/Portafolio-/static/css/main.c2e90358.chunk.css"
+    "revision": "4c2a7c3d4e011139a1bd",
+    "url": "/Portafolio-/static/css/main.23a23c84.chunk.css"
   },
   {
     "revision": "9eee67cf782a38d35aeb",
     "url": "/Portafolio-/static/js/2.d6a534e5.chunk.js"
   },
   {
-    "revision": "64971d68aa9c27f1df2a",
+    "revision": "4c2a7c3d4e011139a1bd",
     "url": "/Portafolio-/static/js/main.d6c1b75e.chunk.js"
   },
   {
