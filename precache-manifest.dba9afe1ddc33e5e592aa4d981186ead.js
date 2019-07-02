1f255789fe13c1349a4da3d29e5a10ac8a93665b
Msaezcardenas <msaezcardenas@gmail.com>
2019-07-02 09:17:02 -0400

    Updates

---
+++ precache-manifest.dba9afe1ddc33e5e592aa4d981186ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a827291383d46edeba7d6990e5eaefc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a1dc435fd81d7070e5",
+    "url": "/static/css/main.a6b9fedc.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "d9a1dc435fd81d7070e5",
+    "url": "/static/js/main.799ec324.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2c74a56e3898990c2886b675abb16556",
+    "url": "/static/media/bici.2c74a56e.jpg"
+  }
+]);
